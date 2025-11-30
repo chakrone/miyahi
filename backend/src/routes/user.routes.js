aa4409951d0ade1c05f8edc08b8fd093ea17bebf
Chakrone Anas <chakrone.anas@gmail.com>
2025-11-30 15:36:31 +0100

    created routes

---
+++ backend/src/routes/user.routes.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const UserController = require("../controllers/user.controller");
+const auth = require("../middleware/auth");
+
+router.get("/me", auth, UserController.getProfile);
+router.put("/me", auth, UserController.updateProfile);
+
+module.exports = router;
